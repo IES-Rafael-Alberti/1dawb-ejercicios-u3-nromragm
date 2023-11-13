250f656b7af29d7e050ef6a8e0abb2f1229df5bc
nromragm <mgarmorn@g.educaand.es>
2023-11-13 20:56:32 +0100

    ª

--- src/3_1_05.py
+++ src/3_1_05.py
@@ -1,12 +1,19 @@
-def lista_inversa(numeros):
+def lista():
+    numeros = list(range(1, 11))
 
-    numeros.sort(reversed)
-    
+    return numeros
+
+
+def lista_inversa(lista):
+    list_inversa = ', '.join(map(str, reversed(lista)))
+    print(list_inversa)
 
 
 def main():
-    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    
+    # Obtener la lista de números
+    numeros = lista()
+
+    # Imprimir la lista en orden inverso separados por comas
     lista_inversa(numeros)
 
 
